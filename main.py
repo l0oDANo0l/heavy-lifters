110f57ef0c7e4a967c58202f292d49957281b82d
Dan Schomburg <dan@Dans-MacBook-Pro.local>
2025-06-12 12:22:19 -0700

    Support turn based convos

--- main.py
+++ main.py
@@ -10,13 +10,18 @@ import sounddevice as sd
 from agents import (
     Agent,
     function_tool,
-    set_tracing_disabled,
 )
 from agents.voice import (
-    AudioInput,
+    StreamedAudioInput,
     SingleAgentVoiceWorkflow,
     VoicePipeline,
 )
+
+IS_RECORDING = False
+CHUNK_LENGTH_S = 0.05  # 100ms
+SAMPLE_RATE = 24000
+FORMAT = np.int16
+CHANNELS = 1
 
 
 @function_tool
@@ -26,40 +31,72 @@ def get_weather(city: str) -> str:
     choices = ["sunny", "cloudy", "rainy", "snowy"]
     return f"The weather in {city} is {random.choice(choices)}."
 
-agent = Agent(
-    name="Assistant",
-    instructions=
-        "You're speaking to a human, so be polite and concise. The conversation should always be in English.",
-    model="gpt-4o-mini",
-    tools=[get_weather],
-)
+
+async def start_voice_pipeline(pipeline, audio_player, audio_input) -> None:
+        try:
+            audio_player.start()
+            result = await pipeline.run(audio_input)
+
+            async for event in result.stream():
+                if event.type == "voice_stream_event_audio":
+                    print(f"event_audio")
+                    audio_player.write(event.data)
+                elif event.type == "voice_stream_event_lifecycle":
+                    print(f"Lifecycle event: {event.event}")
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            audio_player.close()
+
+
+async def send_mic_audio(audio_input) -> None:
+        device_info = sd.query_devices()
+        print(device_info)
+
+        read_size = int(SAMPLE_RATE * 0.02)
+
+        stream = sd.InputStream(
+            channels=CHANNELS,
+            samplerate=SAMPLE_RATE,
+            dtype="int16",
+        )
+        stream.start()
+
+        try:
+            while True:
+                if stream.read_available < read_size:
+                    await asyncio.sleep(0)
+                    continue
+                
+                data, _ = stream.read(read_size)
+
+                await audio_input.add_audio(data)
+                await asyncio.sleep(0)
+        except KeyboardInterrupt:
+            pass
+        finally:
+            stream.stop()
+            stream.close()    
 
 
 async def main():
     
-    # Use the 'VoicePipeline' Which is best for realtime convos. (https://openai.github.io/openai-agents-python/voice/quickstart/)
+    # Use the 'VoicePipeline' which is best for realtime convos. (https://openai.github.io/openai-agents-python/voice/quickstart/)
+    agent = Agent(
+        name="Assistant",
+        instructions=
+            "You're speaking to a human, so be polite and concise. The conversation should always be in English.",
+        model="gpt-4o-mini",
+        tools=[get_weather],
+    )
     pipeline = VoicePipeline(workflow=SingleAgentVoiceWorkflow(agent))
-    player = sd.OutputStream(samplerate=24000, channels=1, dtype=np.int16)
-    player.start()
+    audio_player = sd.OutputStream(samplerate=24000, channels=1, dtype=np.int16)
+    audio_input = StreamedAudioInput()
 
-    duration = 3  # seconds
-    samplerate = 24000
-
-    # TODO: Switch over to detecting audio, and start / stop of conversation!
-    print("Speak now... say something like, what's the weather in portland oregon today?")
-    buffer = sd.rec(int(duration * samplerate), samplerate=samplerate, channels=1, dtype=np.int16)
-    sd.wait()  # Wait until recording is finished
-    print(f"[debug] event: got audio")
-    audio_input = AudioInput(buffer)
-
-    result = await pipeline.run(audio_input)
-
-    async for event in result.stream():
-        print(f"[debug] event: {event.type}")
-        if event.type == "voice_stream_event_audio":
-            # play audio
-            player.write(event.data)
-
+    await asyncio.gather(
+        start_voice_pipeline(pipeline, audio_player, audio_input),
+        send_mic_audio(audio_input)
+    );
 
 if __name__ == "__main__":
     asyncio.run(main())
